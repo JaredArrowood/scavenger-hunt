efa30586f07f8cef5eced409664eed38e56dc80a
Hoa Nguyen <hoowahwin@gmail.com>
2025-03-08 18:05:36 -0600

    inital text implementation for atrium page

---
+++ Code/scavenger_hunt_app/lib/atrium_binary_question.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:scavenger_hunt_app/QuestionScreen2.dart';
+
+class AtriumBinaryQuestion extends StatefulWidget {
+  const AtriumBinaryQuestion({super.key});
+
+  @override
+  State<AtriumBinaryQuestion> createState() => _AtriumBinaryQuestionState();
+}
+
+class _AtriumBinaryQuestionState extends State<AtriumBinaryQuestion> {
+  final TextEditingController _controller = TextEditingController();
+  bool _isCorrect = false;
+
+  //fill this in with a correct answer to the question
+  void _checkAnswer() {
+    if (_controller.text.toLowerCase() == '338') {
+      setState(() {
+        _isCorrect = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Binary 1'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              if (!_isCorrect) ...[
+                Image.asset('assets/atrium_binary.jpg'),
+                Text(
+                  //change this to a question about pft
+                  'Where past and future blend, minds ascend.\n\n Between science and study, my secret extends.\n\n Across from those who built this grand hall,\n\n My numbers await—will you decode them all?\n\n I whisper in ones and zeros, hidden in sight.\n\n Unravel my code to reveal my might.',
+                  style: TextStyle(fontSize: 15),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter your answer',
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _checkAnswer,
+                  child: Text('Submit'),
+                ),
+              ],
+              if (_isCorrect) ...[
+                //can also add an image if one is required
+                // Image.asset('assets/PFT.jpg'),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                        color: Colors.black, width: 2), // Add border here
+                  ),
+                  child: Text(
+                    "YOU GOT IT RIGHT... DESCRIPTION... ",
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: Colors
+                            .green), //change this to a description of the answer
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => QuestionScreen2()));
+                  },
+                  child: Text('Advance to question 2...'),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'You got the question correct!',
+                  style: TextStyle(color: Colors.green, fontSize: 18),
+                ),
+              ],
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
